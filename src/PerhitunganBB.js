90a354752adb4bd6a99a218d4d12137b7f207d92
nurdeafni <nurdeafni@gmail.com>
2019-10-18 03:44:19 -0700

    Awalan

---
+++ src/PerhitunganBB.js
@@ -0,0 +1,71 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+import React from 'react';
+// eslint-disable-next-line prettier/prettier
+import {View, Text,TextInput, Button} from 'react-native';
+
+class PerhitunganBB extends React.Component {
+  state = {
+    beratBadan: 0,
+    TinggiBadan: null,
+    bmi: null,
+    ket: null,
+    Ket: null,
+    ubahTinggikeMeter: null,
+  };
+
+  hitungBMI() {
+    let beratBadan = this.state.beratBadan;
+    let TinggiBadan = this.state.TinggiBadan;
+    let ubahTinggikeMeter = parseFloat(TinggiBadan) / 100;
+    let bmi = (parseFloat(beratBadan) / ubahTinggikeMeter) * ubahTinggikeMeter;
+    if (bmi < 18) {
+      ket = 'Kekurangan Berat Badan';
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+      ket = 'Berat Badan Normal';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      ket = 'Kelebihan Berat Badan';
+    } else if (bmi >= 30) {
+      ket = 'Kegemukan';
+    }
+    this.setState({Ket: ket});
+  }
+  render() {
+    return (
+      <View>
+        <Text>Hitung BMI</Text>
+        <TextInput
+          style={{heigh: 40}}
+          placeholder="Masukan Berat Badan"
+          onChangeText={InputBB => this.setState({beratBadan: InputBB})}
+          value={this.state.beratBadan}
+        />
+        <TextInput
+          placeholder="Masukan Tinggi Badan"
+          style={{
+            heigh: 40,
+            marginBottom: 10,
+            width: 300,
+            backgroundColor: 'white',
+          }}
+          onChangeText={InputTinggi =>
+            this.setState({TinggiBadan: InputTinggi})
+          }
+          value={this.state.TinggiBadan}
+        />
+
+        <Button
+          title="Hitung BMI"
+          onPress={() => {
+            this.hitungBMI();
+          }}
+        />
+        <Text>{this.state.bmi}</Text>
+        <Text>{this.state.Ket}</Text>
+      </View>
+    );
+  }
+}
+
+export default PerhitunganBB;
